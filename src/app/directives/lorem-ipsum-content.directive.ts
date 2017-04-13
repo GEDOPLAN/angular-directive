1174b084ca38cef5888f9fe45c1c09307a66a1f1
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2017-04-13 17:26:33 +0200

    host-Variante und Renderer

--- src/app/directives/lorem-ipsum-content.directive.ts
+++ src/app/directives/lorem-ipsum-content.directive.ts
@@ -1,10 +1,14 @@
-import { Directive, ElementRef, HostBinding, HostListener, Input, OnChanges } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnChanges, Renderer2 } from '@angular/core';
 import * as loremIpsum from 'lorem-ipsum';
 
 
 @Directive({
   selector: '[gedLoremIpsumContent]',
-  exportAs: 'loremIpsumContent'
+  exportAs: 'loremIpsumContent',
+  host: {
+    'class': 'demo',
+    '(mouseenter)': 'mouseEnter()'
+  }
 })
 export class LoremIpsumContentDirective implements OnChanges {
 
@@ -14,8 +18,9 @@ export class LoremIpsumContentDirective implements OnChanges {
   @HostBinding('innerHTML')
   innerHTML: string;
 
-  constructor(element: ElementRef) {
+  constructor(element: ElementRef, renderer: Renderer2) {
     element.nativeElement.style.cursor = 'pointer';
+    renderer.setStyle(element.nativeElement, "border", "1px solid black");
   }
 
   ngOnChanges() {
@@ -29,6 +34,10 @@ export class LoremIpsumContentDirective implements OnChanges {
 
   private generate() {
     this.innerHTML = loremIpsum({ count: this.length });
+  }
+
+  mouseEnter() {
+    console.log("MouseEneter...");
   }
 
 }
